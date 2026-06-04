2dd82ba1878baf0970cb529e4d6f09794ccd66cb
Abhinay Reddy <abhinayreddy738757@gmail.com>
2026-06-04 19:51:23 +0530

    header complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,17 @@
-// import { StrictMode } from 'react'
-import{BrowserRouter, Routes, Route} from 'react-router-dom'
-// import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './loginpage/LoginPage'
-import RegisterPage from './registerpage/RegisterPage.jsx'
-// import './index.css'
-// import App from './App.jsx'
+import RegisterPage from './registerpage/RegisterPage'
+import Header from './header/Header'
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path='/login' element={<LoginPage /> } />
+        <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
-
   )
 }
 
